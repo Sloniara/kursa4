a9ae81223b71ce1f87b0014dd427e7bf2b9c99ab
gorbachev <lalalala040605@gmail.com>
2024-02-18 00:41:34 +0400

    commit2

--- PycharmProjects/pythonProject4/file1.py
+++ PycharmProjects/pythonProject4/file1.py
@@ -5,7 +5,9 @@ def load_operations(file_path):
         return json.load(file)
 
 def mask_card_number(card_number):
-    return f"{card_number[:4]} {'*'*6} {card_number[-4:]}" if card_number else ''
+    return f"{card_number[:4]} **** {card_number[-4:]}" if card_number else ''
+
+
 
 def mask_account_number(account_number):
     return f"**{account_number[-4:]}" if account_number else ''
